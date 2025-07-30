6e99ad52935d00a10654d24958da0e53a94a00fd
Mustafa2 <Mustafauyar91@gmail.com>
2025-07-30 13:20:22 +0300

    m

--- app/src/main/java/com/example/btk_kotlin_1/first.kt
+++ app/src/main/java/com/example/btk_kotlin_1/first.kt
@@ -1,12 +1,37 @@
 package com.example.btk_kotlin_1
 
 fun main(){
-    println("mustafa")
-    integer(127)
+//    println("mustafa")
+    side()
 }
 fun integer(y: Byte=1){
-    var x: Byte = 5
-    var y: Byte = y
+
+    //implicit data type x
+    val x = 5
+
+    //explicit data type y
+    val y: Byte = y
+
     val result = x*y
+
     println("$result")
+}
+fun floating(){
+    val x: Float =2.5f // float icin sonuna f koy
+
+
+}
+
+fun string(){
+    val x: String = "mustafa"
+
+    for (s in x){
+        println(s)
+    }
+    println(x + " uyar".uppercase())
+}
+fun side(){
+    //integer()
+//    floating()
+    string()
 }
