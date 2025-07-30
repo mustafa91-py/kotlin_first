07f0835d329b004191238685ffa33ed729416e59
Mustafa2 <Mustafauyar91@gmail.com>
2025-07-30 12:51:23 +0300

    init

---
+++ app/src/main/java/com/example/btk_kotlin_1/first.kt
@@ -0,0 +1,12 @@
+package com.example.btk_kotlin_1
+
+fun main(){
+    println("mustafa")
+    integer(127)
+}
+fun integer(y: Byte=1){
+    var x: Byte = 5
+    var y: Byte = y
+    val result = x*y
+    println("$result")
+}
